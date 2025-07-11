a964bcf28277474dcd3cf5299bdcf146174c112e
Silhouette <54932288+Silhouette-sophist@users.noreply.github.com>
2025-07-11 22:59:43 +0800

    add

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Silhouette-sophist/callgraph
 
-go 1.22.3
+go 1.23.0
+
+toolchain go1.23.11
+
+require (
+	golang.org/x/mod v0.25.0 // indirect
+	golang.org/x/sync v0.15.0 // indirect
+	golang.org/x/tools v0.34.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 
-	"github.com/Silhouette-sophist/callgraph/service"
+	"golang.org/x/tools/go/packages"
 )
 
 func main() {
 	fmt.Println("callgraph")
-	service.A()
+	cfg := &packages.Config{
+		Mode: packages.NeedTypes | packages.NeedTypesInfo,
+		Dir:  ".",
+	}
+	pkgs, err := packages.Load(cfg, "github.com/Silhouette-sophist/callgraph/service/...")
+	if err != nil {
+		fmt.Printf("packages.Load err %v\n", err)
+		return
+	}
+	fmt.Printf("packages.Load len %d\n", len(pkgs))
+	for _, pkg := range pkgs {
+		fmt.Printf("load pkg info %v\n", pkg)
+	}
 }
